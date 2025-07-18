b602e765067c682b128455de641ed8fcc83ec4a1
Hiroshi Ueki <ueki@sonicgarden.jp>
2025-07-18 10:57:52 +0900

    hamlに対応

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 - git clone
 - アプリ作成
 ```terminal
-rails new app_name -m ./template.rb --css bootstrap --skip-jbuilder --skip-rubocop --skip-test --skip-ci --skip-kamal --skip-solid --skip-devcontainer
+rails new a -m ./template.rb --css bootstrap --skip-jbuilder --skip-rubocop --skip-test --skip-ci --skip-kamal --skip-solid --skip-devcontainer
 ```
+- 途中erb->hamlの確認プロンプトがでるので「y」して続行してね
--- template.rb
+++ template.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
-# 使用方法: rails new app_name -m path/to/this_template.rb --css bootstrap --skip-jbuilder --skip-rubocop --skip-test --skip-ci --skip-kamal --skip-solid --skip-devcontainer
+# 使用方法: rails new a -m ./template.rb --css bootstrap --skip-jbuilder --skip-rubocop --skip-test --skip-ci --skip-kamal --skip-solid --skip-devcontainer
+
+# --- 基本設定 ---
+# 最新Railsバージョンを使用（rails new 時点での最新を利用する想定）
 
 # 不要ファイル削除
 remove_file 'app/views/layouts/mailer.html.erb'
@@ -13,11 +16,8 @@ gsub_file 'Gemfile', /^.*jbuilder.*$/, ''
 gsub_file 'Gemfile', /^.*kamal.*$/, ''
 # tzinfo-data は不要
 gsub_file 'Gemfile', /^.*tzinfo-data.*$/, ''
-
 # rubocop-rails-omakase は不要
-# skip-rubocopしてるから不要かも
-# gsub_file 'Gemfile', /^.*rubocop-rails-omakase.*$/, ''
-
+gsub_file 'Gemfile', /^.*rubocop-rails-omakase.*$/, ''
 # Gemfile のコメント行をすべて削除
 gsub_file 'Gemfile', /^#.*\n/, ''
 
@@ -34,13 +34,16 @@ end
 # simple_form を導入
 gem 'simple_form'
 
+# hamlit-rails と html2haml を導入（変換用）
+gem 'hamlit-rails'
+gem 'html2haml', group: :development
+
 # 必要な gem をインストール
 after_bundle do
   # RSpec 設定
   generate 'rspec:install'
 
   # .rubocop.yml をプロジェクトルートに新規作成
-  say '.rubocop.yml をプロジェクトルートに新規作成', :green
   remove_file '.rubocop.yml' if File.exist?('.rubocop.yml')
   create_file '.rubocop.yml', <<~YAML
     inherit_gem:
@@ -69,6 +72,14 @@ after_bundle do
   if css_file
     append_to_file css_file, "\n@import \"bootstrap/scss/bootstrap\";\n"
   end
+
+  # erb から haml へ変換
+  say 'Converting ERB to Haml...', :green
+  run 'bundle exec rake hamlit:erb2haml'
+
+  # 変換後に html2haml は不要なので Gemfile から削除
+  gsub_file 'Gemfile', /^.*html2haml.*$/, ''
+  run 'bundle install'
 
   git :init
   git add: '.'
